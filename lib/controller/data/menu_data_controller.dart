7755c7cb918482a7aa257dfd4463c4c77ec6056c
VenuraKS25 <vks250102@gmail.com>
2024-07-07 22:33:56 +0530

    Added function to gather all items

--- lib/controller/data/menu_data_controller.dart
+++ lib/controller/data/menu_data_controller.dart
@@ -3,10 +3,13 @@ import 'package:virtual_waiter/controller/network/menu_data_network_controller.d
 
 import '../../model/menu_item.dart';
 
-class MenuDataController extends GetxController{
+class MenuDataController extends GetxController {
   Map _categoryItemMap = {};
   Map<String, List<MenuItem>> _menuItemOrderedMap = {};
   Map<String, List<MenuItem>> get menuItemOrderedMap => _menuItemOrderedMap;
+
+  List<MenuItem> _allItems = [];
+  List<MenuItem> get allItems => _allItems;
 
   final MenuDataNetworkController _mdnc = MenuDataNetworkController.instance;
   static MenuDataController instance = Get.find();
@@ -18,9 +21,9 @@ class MenuDataController extends GetxController{
     _initControllerData();
   }
 
-  void _initControllerData(){
+  void _initControllerData() {
     _categoryItemMap = _mdnc.menuData;
-    if(_categoryItemMap.entries.isEmpty){
+    if (_categoryItemMap.entries.isEmpty) {
       throw Exception('Empty Menu in MenuDataController');
     }
 
@@ -28,12 +31,15 @@ class MenuDataController extends GetxController{
       String category = entry.key;
       List categoryItems = entry.value;
 
-      if(categoryItems.isNotEmpty){
-        List<MenuItem> _items = categoryItems.map((item) => MenuItem.fromJson(item)).toList();
+      if (categoryItems.isNotEmpty) {
+        List<MenuItem> _items = categoryItems.map((item) {
+          MenuItem menuItem = MenuItem.fromJson(item);
+          _allItems.add(menuItem);
+          return menuItem;
+        }).toList();
         _items.sort((a, b) => a.name.compareTo(b.name));
         _menuItemOrderedMap[category] = _items;
       }
     }
   }
-
 }
