572c49fe1a32f9b72221e1ba1594c8567b78aec8
VenuraKS25 <91336404+VenuraKS25@users.noreply.github.com>
2024-10-11 21:33:49 +0530

    edited function parameter types reflecting model changes

--- lib/controller/views/order_screen/order_screen_state_controller.dart
+++ lib/controller/views/order_screen/order_screen_state_controller.dart
@@ -1,77 +1,46 @@
 import 'package:get/get.dart';
-import 'package:virtual_waiter/controller/data/order_data_controller.dart';
+import 'package:virtual_waiter/controller/data/order_item_data_controller.dart';
 import 'package:virtual_waiter/model/order_item.dart';
 
-import '../../../model/menu_item.dart';
-import '../../../model/order_menu_item.dart';
 import '../../data/menu_data_controller.dart';
 
 class OssController extends GetxController {
   final MenuDataController _mdc = MenuDataController.instance;
-  late final OrderDataController _odc;
+  late final OrderItemDataController _odc;
 
-  RxList<OrderMenuItem> _orderMenuItems = <OrderMenuItem>[].obs;
-  List<OrderMenuItem> get orderMenuItems => _orderMenuItems.value;
+  RxList<OrderItem> _orderItems = <OrderItem>[].obs;
+  List<OrderItem> get orderItems => _orderItems;
 
   static OssController instance = Get.find();
 
   RxDouble _totalAmount = 0.0.obs;
   double get totalAmount => _totalAmount.value;
 
-  OrderMenuItem _generateOrderMenuItem({required OrderItem orderItem}) {
-    MenuItem menuItem = _mdc.allItems
-        .where((item) => item.id == orderItem.menuItemId)
-        .toList()
-        .first;
-
-    List<String> addOnList = [];
-
-    orderItem.addOnsQuantity.keys.forEach((addOnId) {
-      addOnList.add(menuItem.addOns
-          .where((addOn) => addOn['id'] == addOnId)
-          .toList()
-          .first['name']);
-    });
-
-    _totalAmount.value += orderItem.totalAmount;
-
-    return OrderMenuItem(
-      itemCategory: menuItem.category,
-      menuItemId: menuItem.id,
-      menuItemName: menuItem.name,
-      menuItemImageUrl: menuItem.imageUrl,
-      itemQuantity: orderItem.itemQuantity,
-      addOnList: addOnList,
-      totalAmount: orderItem.totalAmount,
-    );
-  }
-
   //TODO: Sort orders by category order
   void analyseOrderData() {
-    _orderMenuItems.value = [];
-    _totalAmount.value = 0.0;
-    _odc.orderList.forEach((order) {
-      _orderMenuItems.add(_generateOrderMenuItem(orderItem: order));
-    });
+    _orderItems.value = _odc.orderItemList;
+    _totalAmount.value = _orderItems.fold(
+        0, (previousValue, element) => previousValue + element.totalAmount);
     update();
   }
 
-  void removeOrderItem({required String menuItemId}) {
-    if (_orderMenuItems.value.any((order) => order.menuItemId == menuItemId)) {
-      _orderMenuItems.value
-          .removeWhere((order) => order.menuItemId == menuItemId);
+  void removeOrderItem({required int menuItemId}) {
+    if (_orderItems.any((order) => order.menuItem.id == menuItemId)) {
+      _orderItems
+          .removeWhere((order) => order.menuItem.id == menuItemId);
       _odc.removeOrderItem(menuItemId: menuItemId);
       update();
     }
   }
 
-  OrderItem findOrderItemByMenuItemId({required String id}) {
+  OrderItem findOrderItemByMenuItemId({required int id}) {
     return _odc.findByMenuItemId(id : id);
   }
 
   Future<void> completeOrder () async{
    try{
      await _odc.completeOrder();
+     return;
    }catch(e){
      throw Exception('Cannot complete order');
    }
@@ -84,13 +53,13 @@ class OssController extends GetxController {
   void onReady() {
     // TODO: implement onReady
     super.onReady();
-    ever(_odc.listenableOrderList, _onItemsChanged);
+    ever(_odc.listenableOrderItemList, _onItemsChanged);
   }
 
   @override
   void onInit() {
     // TODO: implement onInit
     super.onInit();
-    _odc = OrderDataController.instance;
+    _odc = OrderItemDataController.instance;
   }
 }
