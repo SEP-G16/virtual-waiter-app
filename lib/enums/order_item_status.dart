beaeacc2d8f40403ad6bf2648e86a258f41409c0
VenuraKS25 <91336404+VenuraKS25@users.noreply.github.com>
2024-10-11 21:30:16 +0530

    new statuses and name generating functionality

--- lib/enums/order_item_status.dart
+++ lib/enums/order_item_status.dart
@@ -1,3 +1,24 @@
 enum OrderItemStatus{
-  Pending, Processing, Ready
+  Pending, Processing, Ready, Editing, Cancelled
+}
+
+extension OrderItemStatusExtension on OrderItemStatus{
+  String toStringAsName()
+  {
+    switch(this)
+    {
+      case OrderItemStatus.Pending:
+        return 'Pending';
+      case OrderItemStatus.Processing:
+        return 'Processing';
+      case OrderItemStatus.Ready:
+        return 'Ready';
+      case OrderItemStatus.Editing:
+        return 'Editing';
+      case OrderItemStatus.Cancelled:
+        return 'Cancelled';
+      default:
+        return 'Editing';
+    }
+  }
 }
