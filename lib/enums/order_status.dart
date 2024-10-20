1c3d35a86bf4232af9f41af381534f07fd482ab7
VenuraKS25 <91336404+VenuraKS25@users.noreply.github.com>
2024-10-20 23:14:07 +0530

    order statuses changed and created function to "convert" between types and strings

--- lib/enums/order_status.dart
+++ lib/enums/order_status.dart
@@ -1,3 +1,43 @@
 enum OrderStatus{
-  Editing, Pending, Processing, Complete
+  Editing, Pending, Processing, Complete, Cancelled, Pending_Payment;
+
+  static OrderStatus fromString(String status){
+    switch(status){
+      case 'Editing':
+        return OrderStatus.Editing;
+      case 'Pending':
+        return OrderStatus.Pending;
+      case 'Processing':
+        return OrderStatus.Processing;
+      case 'Complete':
+        return OrderStatus.Complete;
+      case 'Cancelled':
+        return OrderStatus.Cancelled;
+      case 'Pending_Payment':
+        return OrderStatus.Pending_Payment;
+      default:
+        return OrderStatus.Editing;
+    }
+  }
+}
+
+extension OrderStatusExtension on OrderStatus{
+  String get status{
+    switch(this){
+      case OrderStatus.Editing:
+        return 'Editing';
+      case OrderStatus.Pending:
+        return 'Pending';
+      case OrderStatus.Processing:
+        return 'Processing';
+      case OrderStatus.Complete:
+        return 'Complete';
+      case OrderStatus.Cancelled:
+        return 'Cancelled';
+      case OrderStatus.Pending_Payment:
+        return 'Pending Payment';
+      default:
+        return '';
+    }
+  }
 }
