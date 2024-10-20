eff3af6e8f1d57a4b1098674169381db730ba4bb
VenuraKS25 <91336404+VenuraKS25@users.noreply.github.com>
2024-10-20 22:45:33 +0530

    table data controller modified with preferences controller to use session and jwt

--- lib/controller_initializer.dart
+++ lib/controller_initializer.dart
@@ -2,10 +2,12 @@ import 'package:get/get.dart';
 import 'package:virtual_waiter/controller/data/menu_data_controller.dart';
 import 'package:virtual_waiter/controller/data/order_data_controller.dart';
 import 'package:virtual_waiter/controller/data/order_item_data_controller.dart';
+import 'package:virtual_waiter/controller/data/preferences_controller.dart';
 import 'package:virtual_waiter/controller/data/stream_socket_controller.dart';
 import 'package:virtual_waiter/controller/data/table_data_controller.dart';
 import 'package:virtual_waiter/controller/network/menu_data_network_controller.dart';
 import 'package:virtual_waiter/controller/network/order_data_network_controller.dart';
+import 'package:virtual_waiter/controller/network/table_network_controller.dart';
 import 'package:virtual_waiter/controller/network/web_socket_controller.dart';
 import 'package:virtual_waiter/controller/views/menu_screen/menu_widget_tree_builder.dart';
 import 'package:virtual_waiter/controller/views/order_screen/order_screen_state_controller.dart';
@@ -19,14 +21,25 @@ class ControllerInitializer{
 
   static Future<void> initAllControllers() async {
     try{
-      Get.put(TableDataController());
+      Get.deleteAll();
+
+      final prefCont = Get.put(PreferencesController());
+      await prefCont.setPreference('tableId', 1);
+
+      Get.put(TableNetworkController());
+      final tableDataController = Get.put(TableDataController());
+
+      await tableDataController.initialize();
+
       Get.put(StreamSocket());
       await Get.putAsync(() => WebSocketController.create());
 
       Get.put(MenuDataNetworkController());
       await Get.putAsync(() => MenuDataController.create());
+
       Get.put(OrderDataNetworkController());
-      Get.put(OrderDataController());
+      await Get.putAsync(() => OrderDataController.create());
+
       Get.put(OrderItemDataController());
       Get.put(OssController());
       Get.put(VmisStateController());
@@ -36,7 +49,7 @@ class ControllerInitializer{
 
       _initialized = true;
     }catch(e){
-      print('Error occurred while initialising controllers!');
+      print('Error occurred while initialising controllers! ${e}');
       _initialized = false;
       rethrow;
     }
