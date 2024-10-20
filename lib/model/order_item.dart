76f05a329aa21bfd94f5954650674563217b9301
VenuraKS25 <91336404+VenuraKS25@users.noreply.github.com>
2024-10-20 23:09:13 +0530

    new id property added and created a fromMap method

--- lib/model/order_item.dart
+++ lib/model/order_item.dart
@@ -4,6 +4,7 @@ import 'package:virtual_waiter/model/selected_add_on.dart';
 import '../enums/order_item_status.dart';
 
 class OrderItem {
+  final int id;
   final MenuItem menuItem;
   final int itemQuantity;
   final List<SelectedAddOn> selectedAddOns;
@@ -12,6 +13,7 @@ class OrderItem {
   OrderItemStatus _status = OrderItemStatus.Editing;
 
   OrderItem({
+    required this.id,
     required this.menuItem,
     required this.itemQuantity,
     required this.selectedAddOns,
@@ -24,6 +26,7 @@ class OrderItem {
   set status(OrderItemStatus status) => _status = status;
 
   OrderItem copyWith({
+    int? id,
     MenuItem? menuItem,
     int? itemQuantity,
     List<SelectedAddOn>? selectedAddOns,
@@ -32,6 +35,7 @@ class OrderItem {
     OrderItemStatus? status,
   }) {
     return OrderItem(
+      id: id ?? this.id,
       menuItem: menuItem ?? this.menuItem,
       itemQuantity: itemQuantity ?? this.itemQuantity,
       selectedAddOns: selectedAddOns ?? this.selectedAddOns,
@@ -42,12 +46,24 @@ class OrderItem {
 
   Map<String, dynamic> toMap() {
     return {
-      'menuItemId': menuItem.toMap(),
-      'itemQuantity': itemQuantity,
-      'selectedAddOns': selectedAddOns.map((x) => x.toMap()).toList(),
-      'additionalNote': additionalNote,
+      'id': id,
+      'menuItem': menuItem.toMap(),
+      'quantity': itemQuantity,
+      'addOns': selectedAddOns.map((x) => x.toMap()).toList(),
+      'additionalNotes': additionalNote,
       'totalAmount': totalAmount,
     };
   }
 
+  factory OrderItem.fromMap(Map<String, dynamic> map, MenuItem menuItem, List<SelectedAddOn> addOns) {
+    return OrderItem(
+      id: map['id'],
+      menuItem: menuItem,
+      itemQuantity: map['quantity'],
+      selectedAddOns: addOns,
+      additionalNote: map['additionalNotes'],
+      totalAmount: map['totalAmount'],
+      status: OrderItemStatus.fromString(map['status']),
+    );
+  }
 }
