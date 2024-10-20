b4095c31d91611d1b348aeef58f9f34d5918d711
VenuraKS25 <91336404+VenuraKS25@users.noreply.github.com>
2024-10-20 22:50:09 +0530

    new status instance variable added to store availability status

--- lib/model/menu_item.dart
+++ lib/model/menu_item.dart
@@ -1,6 +1,8 @@
 import 'package:virtual_waiter/model/category.dart';
 import 'package:virtual_waiter/model/add_on.dart';
 
+import '../enums/menu_item_status.dart';
+
 class MenuItem {
   int id;
   String name;
@@ -13,6 +15,7 @@ class MenuItem {
   List<String> tags;
   String imageUrl;
   List<AddOn> addOns;
+  MenuItemStatus _status;
 
   MenuItem({
     required this.id,
@@ -24,9 +27,15 @@ class MenuItem {
     required this.price,
     required this.cuisine,
     required this.tags,
-    this.imageUrl = 'https://saltedmint.com/wp-content/uploads/2024/01/Vegetable-Spring-Rolls-3.jpg',
+    this.imageUrl =
+        'https://saltedmint.com/wp-content/uploads/2024/01/Vegetable-Spring-Rolls-3.jpg',
     this.addOns = const [],
-  });
+    MenuItemStatus status = MenuItemStatus.InStock,
+  }) : this._status = status;
+
+  MenuItemStatus get status => _status;
+
+  set status(MenuItemStatus status) => _status = status;
 
   Map<String, dynamic> toMap() {
     return {
@@ -55,8 +64,41 @@ class MenuItem {
       price: map['price'],
       cuisine: map['cuisine'],
       tags: List<String>.from(map['tags']),
-      imageUrl: map['imageUrl'] ?? 'https://saltedmint.com/wp-content/uploads/2024/01/Vegetable-Spring-Rolls-3.jpg',
-      addOns: map['addOns'] != null ? List<AddOn>.from(map['addOns'].map((addOn) => AddOn.fromMap(addOn))) : [],
+      imageUrl: map['imageUrl'] ??
+          'https://saltedmint.com/wp-content/uploads/2024/01/Vegetable-Spring-Rolls-3.jpg',
+      addOns: map['addOns'] != null
+          ? List<AddOn>.from(map['addOns'].map((addOn) => AddOn.fromMap(addOn)))
+          : [],
+    );
+  }
+
+  MenuItem copyWith({
+    int? id,
+    String? name,
+    String? shortDescription,
+    String? longDescription,
+    Category? category,
+    List<String>? ingredients,
+    double? price,
+    String? cuisine,
+    List<String>? tags,
+    String? imageUrl,
+    List<AddOn>? addOns,
+    MenuItemStatus? status,
+  }) {
+    return MenuItem(
+      id: id ?? this.id,
+      name: name ?? this.name,
+      shortDescription: shortDescription ?? this.shortDescription,
+      longDescription: longDescription ?? this.longDescription,
+      category: category ?? this.category,
+      ingredients: ingredients ?? this.ingredients,
+      price: price ?? this.price,
+      cuisine: cuisine ?? this.cuisine,
+      tags: tags ?? this.tags,
+      imageUrl: imageUrl ?? this.imageUrl,
+      addOns: addOns ?? this.addOns,
+      status: status ?? this._status,
     );
   }
 }
